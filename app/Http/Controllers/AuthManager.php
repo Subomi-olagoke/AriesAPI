0727ce7021520a049f03366dde2e10f34b472122
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-20 21:20:14 +0100

    done with logging and registration

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Response;
 
 class AuthManager extends Controller
 {
-    //
+    function register(Request $request, Response $response) {
+        $incomingFields = $request->validate([
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:20', 'confirmed']
+        ]);
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return response()->json($response);
+       // return redirect('/');
+    }
+
+
+    //login
+    function login(Request $request, Response $response) {
+        $incomingFields = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if(auth()->attempt(['email' => $incomingFields['email'],'password' => $incomingFields['password'] ])) {
+            return response()->json($response);
+           // $request->session()->regenerate();
+            //return redirect('/');
+
+        } /*else {
+            return redirect('/');
+        }*/
+
+    }
+
+   /* public function showCorrectHomePage() {
+        if (auth()->check()) {
+            return route('homepage-feed');
+        } else {
+            return route('homepage');
+        }
+
+    }*/
 }
