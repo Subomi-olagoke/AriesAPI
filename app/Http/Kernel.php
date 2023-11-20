ae6b555151696ac3c5656b2f0821a4074ef2a2bd
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-20 21:20:33 +0100

    cross check

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,8 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+\Illuminate\Session\Middleware\StartSession::class;
+\Illuminate\View\Middleware\ShareErrorsFromSession::class;
 
 class Kernel extends HttpKernel
 {
