e38160c74ae72d3e909ae7394445cbfe298825f7
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-21 22:35:16 +0100

    now loading posts and courses to user feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use App\Models\Topic;
 use App\Models\Courses;
 use Illuminate\Http\Request;
@@ -11,9 +13,23 @@ class FeedController extends Controller
     public function feed(Request $request) {
         $user = auth()->user();
         $topicIds = $user->topic()->pluck('id');
-
         $courses = Courses::whereIn('topic_id', $topicIds)->get();
 
-        return $courses;
+        $posts = Post::with('user')
+        ->whereIn('user_id', $user->following()->pluck('id'))
+        ->latest()
+        ->get();
+
+        if($user->role == User::ROLE_LEARNER) {
+            return response()->json([
+                'posts' => $posts,
+                'courses' => $courses
+            ]);
+        }
+
+        return response()->json([
+            'posts' => $posts
+        ]);
+
     }
 }
