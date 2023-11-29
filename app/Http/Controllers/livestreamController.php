5ab1116d20b3de87dca7696ef714df8b2c5a7fc2
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-29 21:25:15 +0100

    testing conn between Go and laravel

--- app/Http/Controllers/livestreamController.php
+++ app/Http/Controllers/livestreamController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class livestreamController extends Controller
 {
-    //
+    public function someAction()
+    {
+        $response = Http::get("http://localhost:44123/test");
+
+        // Process the response from Go
+        $data = $response->json();
+        return $data;
+    }
+
 }
