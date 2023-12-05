9dfdd51b8d1ed2d55744804bc76cc1b921a88118
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-05 11:10:55 +0100

    Finished profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,33 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Profile;
 
+class ProfileController extends Controller {
+	public function showProfile(User $profile) {
+		$user = Auth::user();
+		$profile = Profile::where('user_id', $user->id)->first();
+		return response()->json(['profile' => $profile]);
+	}
 
-class ProfileController extends Controller
-{
-    public function showProfile(User $profile) {
-        $user = Auth::user();
-        $profile = Profile::where('user_id', $user->id)->first();
-        return response()->json(['profile' => $profile]);
-    }
+	public function update(Request $request) {
+		$user = Auth::user();
+		$profile = Profile::where('user_id', $user->id)->first();
 
-    public function update(Request $request)
-    {
-        $user = Auth::user();
-        $profile = Profile::where('user_id', $user->id)->first();
+		if (!$profile) {
+			$profile = new Profile(['user_id' => $user->id]);
+		}
 
-        if (!$profile) {
-            $profile = new Profile(['user_id' => $user->id]);
-        }
+		$profile->fill($request->all());
+		$profile->save();
 
-        $profile->fill($request->all());
-        $profile->save();
-
-        return response()->json(['profile' => $profile]);
-    }
+		return response()->json(['profile' => $profile]);
+	}
 
 }
