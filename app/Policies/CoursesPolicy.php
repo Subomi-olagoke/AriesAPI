227a47e075f6193e6686d48c09779540c8a12e61
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-05 11:09:54 +0100

    Admin privileges for courses

--- app/Policies/CoursesPolicy.php
+++ app/Policies/CoursesPolicy.php
@@ -4,63 +4,75 @@ namespace App\Policies;
 
 use App\Models\Courses;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
-class CoursesPolicy
-{
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
+class CoursesPolicy {
+	/**
+	 * Determine whether the user can view any models.
+	 */
+	public function viewAny(User $user): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Courses $courses): bool
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can view the model.
+	 */
+	public function view(User $user, Courses $courses): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can create models.
+	 */
+	public function create(User $user): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Courses $courses): bool
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can update the model.
+	 */
+	public function update(User $user, Courses $courses): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Courses $courses): bool
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can delete the model.
+	 */
+	public function delete(User $user, Courses $courses): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Courses $courses): bool
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can restore the model.
+	 */
+	public function restore(User $user, Courses $courses): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Courses $courses): bool
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can permanently delete the model.
+	 */
+	public function forceDelete(User $user, Courses $courses): bool {
+		if ($user->isAdmin === 1) {
+			return true;
+		}
+		return $user->id === $post->user_id;
+	}
 }
