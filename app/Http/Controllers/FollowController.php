f5c3443467a111fd6d70848d2d54c62f7da92188
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-14 03:31:03 +0100

    user follow function for follow feature

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Follow;
+use App\Models\User;
+
+class FollowController extends Controller {
+	public function createFollow(User $user) {
+		// cannot follow self
+        if($user->id == auth()->user()->id) {
+            return back()->with('failure', 'You cannot follow yourself')
+
+        }
+		//cannot follow already followed user
+        $existCheck = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
+        if($existCheck) {
+            return back()->with('failure', 'You are already following this user');
+        }
+
+		$newFollow = new Follow;
+		$newFollow->user_id = auth()->user()->id;
+		$newFollow->followeduser = $user->id;
+		$newFollow->save();
+
+        back()->with('success', 'User successfully followed');
+
+	}
+	public function unFollow() {
+
+	}
+}
