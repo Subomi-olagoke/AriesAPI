09c976569de6c1d25913bab1ef567de25cf51683
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-14 03:32:59 +0100

    Done with profiles, moving to other features

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,10 +8,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller {
-	public function showProfile(User $profile) {
-		$user = Auth::user();
+	/*public function showProfile(User $user) {
+		//$user = Auth::user();
 		$profile = Profile::where('user_id', $user->id)->first();
 		return response()->json(['profile' => $profile]);
+	}*/
+
+	public function showProfile(User $user) {
+		$data = [
+			'username' => $user->username,
+			'posts' => $user->posts()->latest()->get(),
+			'postCount' => $user->posts()->count(),
+
+		];
+
+		return response()->json(['data' => $data]);
 	}
 
 	public function update(Request $request) {
