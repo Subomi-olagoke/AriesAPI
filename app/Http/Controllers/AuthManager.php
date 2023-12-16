a59efa8c7e6f346869b5eb0c2f29e93114b2c01e
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-16 05:29:39 +0100

    educator page fix for roles

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -6,78 +6,78 @@
 
 namespace App\Http\Controllers;
 
-use Response;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use Illuminate\Auth\Events\Verified;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\RedirectResponse;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use Response;
 
-class AuthManager extends Controller
-{
-    function register(Request $request, Response $response) {
-        $incomingFields = $request->validate([
-            'firstName' => ['required',],
-            'LastName' => ['required'],
-            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
-            'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8', 'max:20', 'confirmed'],
-            'role' => ['required', Rule::in(['educator', 'student', 'Admin'])]
-        ]);
-       /*
-       if ($incomingFields['role'] == 'educator') {
-            return redirect()->route('Educator.reg');
-        }*/
-        $incomingFields['password'] = bcrypt($incomingFields['password']);
+class AuthManager extends Controller {
+	function register(Request $request, Response $response) {
+		$incomingFields = $request->validate([
+			'firstName' => ['required'],
+			'LastName' => ['required'],
+			'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+			'email' => ['required', 'email', Rule::unique('users', 'email')],
+			'password' => ['required', 'min:8', 'max:20', 'confirmed'],
+			'role' => ['required', Rule::in(['educator', 'student', 'Admin'])],
+		]);
+		/*
+			       if ($incomingFields['role'] == 'educator') {
+			            return redirect()->route('Educator.reg');
+		*/
+		if ($incomingFields['role'] == 'educator') {
+			$response['next_step'] = 'educator_registration';
+		}
 
-        $user = User::create($incomingFields);
-        auth()->login($user);
-        return response()->json($response);
-       // return redirect('/');
-    }
+		return response()->json($response, 201);
 
+		$incomingFields['password'] = bcrypt($incomingFields['password']);
 
-    //login
-    function login(Request $request, Response $response) {
-        $incomingFields = $request->validate([
-            'email' => 'required',
-            'password' => 'required'
-        ]);
-        if(auth()->attempt(['email' => $incomingFields['email'],'password' => $incomingFields['password'] ])) {
-            return response()->json($response);
+		$user = User::create($incomingFields);
+		auth()->login($user);
+		return response()->json($response);
+		// return redirect('/');
+	}
 
-        }
+	//login
+	function login(Request $request, Response $response) {
+		$incomingFields = $request->validate([
+			'email' => 'required',
+			'password' => 'required',
+		]);
+		if (auth()->attempt(['email' => $incomingFields['email'], 'password' => $incomingFields['password']])) {
+			return response()->json($response);
 
-    }
+		}
 
+	}
 
-    //logout
-    public function destroy(Request $request): RedirectResponse
-    {
-        Auth::guard('web')->logout();
+	//logout
+	public function destroy(Request $request): RedirectResponse {
+		Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
+		$request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+		$request->session()->regenerateToken();
 
-        return redirect('Homefeed');
-    }
+		return redirect('Homefeed');
+	}
 
+	public function __invoke(EmailVerificationRequest $request): RedirectResponse {
+		if ($request->user()->hasVerifiedEmail()) {
+			return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
+		}
 
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
-    {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
-        }
+		if ($request->user()->markEmailAsVerified()) {
+			event(new Verified($request->user()));
+		}
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
-
-        return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
-    }
+		return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
+	}
 
 }
