fd60496c78be6954be616bb769fbfdcbc4334763
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-21 05:05:25 +0100

    added crud functions for avatars feature in profiles

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller {
 	/*public function showProfile(User $user) {
@@ -37,6 +38,30 @@ class ProfileController extends Controller {
 		$profile->save();
 
 		return response()->json(['profile' => $profile]);
+	}
+
+	public function UploadAvatar(Request $request) {
+		$user = auth()->user();
+
+		$this->validate($request, [
+			'avatar' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+		]);
+
+		if ($request->hasFile('avatar')) {
+			$avatar = $request->file('avatar');
+
+			$filename = 'avatar_' . time() . '.' . $avatar->getClientOriginalExtension();
+
+			Storage::disk('s3')->put('avatars/' . $filename, file_get_contents($avatar));
+
+			$user->avatar = $filename;
+			$user->save();
+
+			return response()->json(['message' => 'Avatar uploaded and saved successfully']);
+		}
+
+		return response()->json(['message' => 'No avatar provided'], 400);
+
 	}
 
 }
