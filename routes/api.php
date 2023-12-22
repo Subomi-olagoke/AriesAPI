50dd1b810dc21127d6bef8dcd876f7545ba755fb
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-22 07:11:09 +0100

    added comments route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthManager;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\EducatorsController;
 use App\Http\Controllers\FollowController;
 use App\Http\Controllers\ForgotPasswordManager;
 use App\Http\Controllers\livestreamController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
@@ -39,34 +41,34 @@ Route::post('resetPassword', [ResetPasswordController::class, 'resetPassword'])-
 
 //Profile related routes
 Route::get('/profile/{user:username}', [ProfileController::class, 'showProfile']); //->middleware('checkAuth');
-Route::post('/manage-avatar', [ProfileController::class, 'manage-avatar'])->middleware('mustBeLoggedIn');
+Route::post('/uploadAvatar', [ProfileController::class, 'UploadAvatar'])->middleware('mustBeLoggedIn');
 
 //follow related routes
 Route::post('/create-follow/{user:username}', [FollowController::class, 'createFollow'])->middleware('mustBeLoggedIn');
 Route::post('/unfollow/{user:username}', [FollowController::class, 'unFollow'])->middleware('mustBeLoggedIn');
 
 // Blog post related routes
-Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
+Route::post('/storeNewPost', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
 
 //courses
 //post a course
-Route::post('/PostCourse', [CoursesController::class, 'PostCourse'])->middleware->('mustBeLoggedIn');
+Route::post('/PostCourse', [CoursesController::class, 'PostCourse'])->middleware('mustBeLoggedIn');
 // Show a specific course
 Route::get('/courses/{id}', [CoursesController::class, 'showCourse']);
 //update course
-Route::post('/updateCourse', [CoursesController::class, 'updateCourse'])->middleware->('mustBeLoggedIn');
+Route::post('/updateCourse', [CoursesController::class, 'updateCourse'])->middleware('mustBeLoggedIn');
 
 // Delete a specific course
-Route::delete('/courses/{id}', [CoursesController::class, 'deleteCourse'])->middleware->('mustBeLoggedIn');
+Route::delete('/courses/{id}', [CoursesController::class, 'deleteCourse'])->middleware('mustBeLoggedIn');
 
-
-
+//Commenting on posts
+Route::post('/comment', [CommentController::class, 'postComment']);
 
 /*Route::post('/Upload', [EducatorsController::class, 'upload'])->middleware('mustBeLoggedIn');
 Route::post('/download{file}', [EducatorsController::class, 'download'])->middleware('mustBeLoggedIn');
 Route::get('/show', [EducatorsController::class, 'show']);
-*/
+ */
 
 //livestream
 Route::get('/test', [livestreamController::class, 'someAction']);
