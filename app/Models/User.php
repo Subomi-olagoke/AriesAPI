b8cf8e904742ad77658b16498a1476429faccf2c
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-30 22:51:42 +0100

    added tokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class User extends Authenticatable {
 	use HasApiTokens, HasFactory, Notifiable;
@@ -44,15 +45,19 @@ class User extends Authenticatable {
 		'password' => 'hashed',
 	];
 
-	public function courses() {
-		return $this->hasMany(Courses::class, 'user_id');
-	}
+	// public function courses() {
+	// 	return $this->hasMany(Courses::class, 'user_id');
+	// }
 
-	public function getRouteKeyName() {
-		return 'username';
-	}
-	public function posts() {
-		return $this->hasMany(Post::class, 'user_id');
+	// public function getRouteKeyName() {
+	// 	return 'username';
+	// }
+	// public function posts() {
+	// 	return $this->hasMany(Post::class, 'user_id');
+	// }
+
+	public function tokens() {
+		return $this->hasMany(SanctumPersonalAccessToken::class);
 	}
 
 }
