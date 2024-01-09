170918cbc776a22b580b5a740db2843ba957314a
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-01-09 22:20:09 +0100

    moiddleware configs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel {
 
 		'api' => [
 			//\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-			\Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+			'throttle:api',
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 		],
 	];
@@ -68,6 +68,8 @@ class Kernel extends HttpKernel {
 		'signed' => \App\Http\Middleware\ValidateSignature::class,
 		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 		'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+		'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
+		'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
 	];
 
 	protected $routeMiddleware = [
