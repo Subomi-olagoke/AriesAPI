10013a83520f0e7a015cc2fa2114bf8128170f01
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-11-04 20:58:41 +0100

    model relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,66 +2,69 @@
 
 namespace App\Models;
 
-use App\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class User extends Authenticatable {
-	use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $fillable = [
-		'username',
-		'email',
-		'password',
-		'firstName',
-		'LastName',
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'firstName',
+        'LastName',
+        'role'
+    ];
 
-	/**
-	 * The attributes that should be hidden for serialization.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $hidden = [
-		'password',
-		'remember_token',
-	];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-	/**
-	 * The attributes that should be cast.
-	 *
-	 * @var array<string, string>
-	 */
-	protected $casts = [
-		'email_verified_at' => 'datetime',
-		'password' => 'hashed',
-	];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
-	// public function courses() {
-	// 	return $this->hasMany(Courses::class, 'user_id');
-	// }
+    // public function courses() {
+    //  return $this->hasMany(Courses::class, 'user_id');
+    // }
 
-	public function getRouteKeyName() {
-		return 'username';
-	}
-	public function posts() {
-		return $this->hasMany(Post::class, 'user_id');
-	}
+    public function getRouteKeyName() {
+        return 'username';
+    }
 
-	public function profile() {
-		return $this->belongsTo(Profile::class, 'user_id');
-	}
+    public function posts() {
+        return $this->hasMany(Post::class, 'user_id');
+    }
 
-	public function tokens() {
-		return $this->hasMany(SanctumPersonalAccessToken::class);
-	}
+    public function profile() {
+        return $this->belongsTo(Profile::class, 'user_id');
+    }
 
+    public function likes() {
+        return $this->belongsTo(Like::class, 'user_id');
+    }
+
+
+
+    // Remove the tokens method
 }
