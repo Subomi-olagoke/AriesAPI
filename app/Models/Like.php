6b6850d54f0c6bf4cee7d21d010a53809824e9c5
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-11-13 22:34:35 +0100

    fixed relationship

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,9 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id','likeable_id', 'likeable_type'];
 
-    public function User() {
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user() {
 		return $this->belongsTo(User::class, 'user_id');
 	}
+
+    // public function post() {
+    //     return $this->belongsTo(Post::class, 'likeable_id');
+    // }
+
+    // public function comments() {
+    //     return $this->belongsTo(comments::class, 'comment_id');
+    // }
 
 }
