69d16e69c65e9eb943c94eb2957c96022e998837
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-12-22 17:05:19 +0100

    added suggested courses and top courses features to user feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Topic;
+use App\Models\Course;
 use App\Models\Courses;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
 {
-    public function feed(Request $request) {
+    public function feed() {
         $user = auth()->user();
         $topicIds = $user->topic()->pluck('id');
-        $courses = Courses::whereIn('topic_id', $topicIds)->get();
+        $courses = Course::whereIn('topic_id', $topicIds)->get();
 
         $posts = Post::with('user')
         ->whereIn('user_id', $user->following()->pluck('id'))
@@ -32,4 +34,30 @@ class FeedController extends Controller
         ]);
 
     }
+
+    public function suggestedCourses() {
+        $user = auth()->user();
+        $followedId = $user->following()->pluck('id');
+        $peerLikes = Like::whereIn('user_id', $followedId)
+                    ->whereNotNull('course_id')
+                    ->pluck('course_id');
+
+        $courses = Course::whereIn('id', $peerLikes)->get();
+        return response()->json([
+            'suggested_courses' => $courses
+        ],200);
+
+    }
+
+    public function topCourses() {
+        $topCourses = Course::withCount('likes')
+            ->orderBy('likes_count', 'description')
+            ->take(10)
+            ->get();
+
+        return response()->json([
+            'top_courses' => $topCourses,
+        ]);
+    }
+
 }
