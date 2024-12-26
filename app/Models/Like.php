f7da04c4574f1620e6d432842c2fd026257428e9
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-12-26 22:26:21 +0100

    restructured like relationships

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,14 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id','likeable_id', 'likeable_type'];
-
-    public function likeable()
-    {
-        return $this->morphTo();
-    }
+    protected $fillable = ['user_id'];
 
     public function user() {
 		return $this->belongsTo(User::class, 'user_id');
 	}
+
+    public function post() {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+
+    public function comment() {
+        return $this->belongTo(Comment::class, 'comment_id');
+    }
+
+    public function course() {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
 }
