a78e3e7bc0f1f9383b6c207613ab641f5f0229f8
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2025-02-02 20:09:12 +0100

    fixed namespace error

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Workbench\App\Models\User;
+use App\Models\User;
 
 class SearchController extends Controller
 {
