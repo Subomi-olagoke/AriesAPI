415f4e4df03c2f8883c30784d0c0a1b31512d509
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2025-02-02 12:31:39 +0100

    added notif id and type

---
+++ database/migrations/2025_02_02_112928_add_notifiable_columns_to_notifications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->uuid('notifiable_id'); // Add the notifiable_id column
+            $table->string('notifiable_type'); // Add the notifiable_type column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropColumn('notifiable_id'); // Remove the notifiable_id column
+        $table->dropColumn('notifiable_type'); // Remove the notifiable_type column
+        });
+    }
+};
