9c8e5dde5db072be9cd76a9682326bc97afeb8c0
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2025-02-10 20:14:29 +0100

    removed user_id (unnecessary)

--- database/migrations/2025_01_28_102832_create_hire_requests_table.php
+++ database/migrations/2025_01_28_102832_create_hire_requests_table.php
@@ -17,8 +17,6 @@ return new class extends Migration
             $table->uuid('tutor_id');
             $table->enum('status', ['pending', 'accepted', 'declined'])->default('pending');
             $table->text('message')->nullable();
-            $table->uuid('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
