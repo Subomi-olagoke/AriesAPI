815d7d5219746d4a9adbaf90c7afb060845a2fb0
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2025-02-16 19:36:50 +0100

    notifcations route

--- routes/api.php
+++ routes/api.php
@@ -90,6 +90,9 @@ Route::prefix('api')->middleware(['auth:sanctum'])->group(function() {
     Route::patch('/hire-request/{id}/decline', [HireRequestController::class, 'declineRequest']);
     Route::get('/hire-requests', [HireRequestController::class, 'getRequests']);
 
+    Route::get('/notifications', [NotificationController::class, 'getNotifications']);
+    Route::post('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
+
     //setup status
     Route::get('/setup_status', [SetupController::class, 'checkSetupStatus']);
 
