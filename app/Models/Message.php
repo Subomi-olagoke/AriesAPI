e29942b591440db2034d0757883d69de4ae6a2bf
Subomi-olagoke <solagoke21@gmail.com>
2025-03-11 13:39:38 +0100

    Messages Model Fixes

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -1,16 +1,23 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Message extends Model
 {
     use HasFactory, SoftDeletes;
 
+    // Disable auto-incrementing ID
+    public $incrementing = false;
+    
+    // Set ID type to string
+    protected $keyType = 'string';
+
     protected $fillable = [
+        'id',
         'conversation_id',
         'sender_id',
         'body',
@@ -20,8 +27,21 @@ class Message extends Model
     ];
 
     protected $casts = [
+        'id' => 'string',
         'is_read' => 'boolean',
     ];
+
+    // Automatically generate UUID when creating a new message
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 
     /**
      * Get the conversation that owns the message.
