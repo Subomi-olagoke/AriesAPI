837dcfc9675f6ef5a50ef281411d8eefe9d56cf3
Subomi-olagoke <solagoke21@gmail.com>
2025-03-11 13:53:24 +0100

    ..

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -5,7 +5,7 @@ namespace App\Events;
 use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -46,5 +46,15 @@ class MessageSent implements ShouldBroadcast
     public function broadcastAs(): string
     {
         return 'message.received';
+    }
+
+    /**
+     * Get the data to broadcast.
+     */
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => $this->message->load('sender')
+        ];
     }
 }
